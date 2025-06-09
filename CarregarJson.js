c9e90deb647b42ee11b19435f0e60b3ce85871ef
Akil23-alt <kauaakil23@gmail.com>
2025-06-08 23:02:14 -0300

    Update CarregarJson.js

--- CarregarJson.js
+++ CarregarJson.js
@@ -19,10 +19,8 @@ if (novosProdutosParaAdicionar.length > 0) {
   listaProdutosAtual = listaProdutosAtual.concat(novosProdutosParaAdicionar);
   localStorage.setItem("listaProdutos", JSON.stringify(listaProdutosAtual));
   console.log(`${novosProdutosParaAdicionar.length} produtos do JSON foram adicionados ao localStorage.`);
-  carregarGraficos(); 
 } else {
   console.log('Nenhum novo produto do JSON para adicionar (já existem ou o JSON está vazio).');
-  carregarGraficos();
 }
   } catch (error) {
     console.error("Houve um erro ao carregar ou processar o JSON de produtos iniciais:", error);
